ff56c17a246030c7a7ed5463c21a16a34399166e
CelebroPensante <vitor.gustavo.jc@gmail.com>
2025-06-03 21:27:46 -0300

    first commit

--- main.c
+++ main.c
@@ -1,3 +1,12 @@
+//-------------------------------------------------------
+// Aluno: Vitor G. J. de Carvalho                       |
+// RGM: 33046794                                        |
+// Disciplina: Estruturas de Dados avancadas            |
+// Professor: Marcelo T. Uemura                         |
+// Data: 03/06/2025                                     |
+//-------------------------------------------------------
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
